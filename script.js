97bb04a34c7b7c6fe97841697d4751386535d5e5
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-14 01:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+
+	let para = {
+		0: "0",
+		1: "1",
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz"
+	};
+
+	let num = Number(input_digit);
 }
 
 module.exports = letterCombinations;
