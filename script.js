60d2ad323658953b52f01126f3eec3a883d30558
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-15 08:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
 
-	let para = {
-		0: "0",
-		1: "1",
-		2: "abc",
-		3: "def",
-		4: "ghi",
-		5: "jkl",
-		6: "mno",
-		7: "pqrs",
-		8: "tuv",
-		9: "wxyz"
-	};
+	let table = {
+        '0': "0",
+        '1': "1",
+        '2': "abc",
+        '3': "def",
+        '4': "ghi",
+        '5': "jkl",
+        '6': "mno",
+        '7': "pqrs",
+        '8': "tuv",
+        '9': "wxyz"
+    }; 
 
-	let num = Number(input_digit);
+    let sol = [];
+
+    function solve(curIndex, calcstr){
+        if(curIndex === input.length){
+            sol.push(calcstr);
+            return;
+        }
+
+        let curchar = input[curIndex];
+        for(let i = 0; i < table[curchar].length; i++){
+            solve(curIndex + 1, calcstr + table[curchar][i]);
+        }
+    }
+
+    solve(0, "");
+
+    return sol;
 }
 
 module.exports = letterCombinations;
